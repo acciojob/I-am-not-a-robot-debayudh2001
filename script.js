cd91539287c062eb283155a6bae3739984e0df43
debayudh2001 <152640749+debayudh2001@users.noreply.github.com>
2025-02-03 18:23:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your code here
+let h1 = document.createElement("h1")
+h1.innerText = "I'm not a robot"
+h1.style.textAlign = "center"
+let images = []
+for(let i=1; i<=5; i++){
+	let img = document.createElement("img")
+	img.className = "img" + i
+	images.push(img)
+}
+let main = document.querySelector("main")
+main.append(h1)
+let randomImg = images[parseInt(Math.random()*images.length)]
+let img6 = document.createElement("img")
+img6.className = randomImg.className
+images.push(img6)
+images.sort((x,y) => 0.5 - Math.random())
+for(let v of images){
+	main.append(v)
+}
+let h3 = document.createElement("h3")
+h3.innerText = "Please click on the identical tiles to verify that you are not a robot."
+main.append(h3)
+
+for(let i=0; i< images.length; i++){
+	images[i].addEventListener("click", (e) => {
+		let img = e.target
+		map.set(i, images[i])
+	})
+}
